ff72e6b9b1362781c474867e9dbd3ecc575663b7
Rachit Trivedi <technoprogramming92@gmail.com>
2024-04-08 15:09:19 +0530

    before-after-slider1

--- js/before-after.js
+++ js/before-after.js
@@ -1,4 +1,19 @@
-const container1 = document.querySelector(".container1");
-document.querySelector(".slider").addEventListener("input", (e) => {
-  container1.style.setProperty("--position", `${e.target.value}%`);
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all the containers
+  let containers = document.querySelectorAll(".images-container");
+
+  containers.forEach(function (container) {
+    let slider = container.querySelector(".slider-line");
+    let afterImage = container.querySelector(".after-image");
+
+    container.addEventListener("mousemove", function (e) {
+      let rect = container.getBoundingClientRect();
+      let x = e.clientX - rect.left; // Get the horizontal coordinate within the container
+      let width = container.offsetWidth;
+      if (x < 0) x = 0;
+      if (x > width) x = width;
+      slider.style.left = x + "px";
+      afterImage.style.clip = "rect(0, " + x + "px, auto, auto)";
+    });
+  });
 });
